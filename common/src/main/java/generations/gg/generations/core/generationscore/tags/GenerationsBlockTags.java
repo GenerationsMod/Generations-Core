5a148e0e880de67765261e0e6c0d36efe8201731
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-05-08 14:39:51 -0400

    Decrease code in GenerationsBlockTags.java
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- common/src/main/java/generations/gg/generations/core/generationscore/tags/GenerationsBlockTags.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/tags/GenerationsBlockTags.java
@@ -2,31 +2,30 @@ package generations.gg.generations.core.generationscore.tags;
 
 import generations.gg.generations.core.generationscore.GenerationsCore;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class GenerationsBlockTags {
-    public static final TagKey<Block> POKEBRICKS = create(GenerationsCore.id("pokebricks"));
-    public static final TagKey<Block> GENERATIONSORES = create(GenerationsCore.id("generationsores"));
-    public static final TagKey<Block> SHRINES = create(GenerationsCore.id("shrines"));
-    public static final TagKey<Block> MARBLE = create(GenerationsCore.id("marble"));
+    public static final TagKey<Block> POKEBRICKS = create("pokebricks");
+    public static final TagKey<Block> GENERATIONSORES = create("generationsores");
+    public static final TagKey<Block> SHRINES = create("shrines");
+    public static final TagKey<Block> MARBLE = create("marble");
 
-    public static final TagKey<Block> ALUMINUM_ORES = create(GenerationsCore.id("aluminum_ores"));
-    public static final TagKey<Block> SAPPHIRE_ORES = create(GenerationsCore.id("sapphire_ores"));
-    public static final TagKey<Block> RUBY_ORES = create(GenerationsCore.id("ruby_ores"));
-    public static final TagKey<Block> SILICON_ORES = create(GenerationsCore.id("silicon_ores"));
-    public static final TagKey<Block> Z_CRYSTAL_ORES = create(GenerationsCore.id("z_crystal_ores"));
-    public static final TagKey<Block> FOSSIL_ORES = create(GenerationsCore.id("fossil_ores"));
-    public static final TagKey<Block> ULTRA_DARK_LOGS = create(GenerationsCore.id("ultra_dark_logs"));
-    public static final TagKey<Block> ULTRA_JUNGLE_LOGS = create(GenerationsCore.id("ultra_jungle_logs"));
-    public static final TagKey<Block> GHOST_LOGS = create(GenerationsCore.id("ghost_logs"));
-    public static final TagKey<Block> CHARGE_STONE_BRICKS = create(GenerationsCore.id("charge_stone_bricks"));
-    public static final TagKey<Block> VOLCANIC_STONE_BRICKS = create(GenerationsCore.id("volcanic_stone_bricks"));
-    public static final TagKey<Block> POKEBALL_CHESTS = create(GenerationsCore.id("pokeball_chests"));
+    public static final TagKey<Block> ALUMINUM_ORES = create("aluminum_ores");
+    public static final TagKey<Block> SAPPHIRE_ORES = create("sapphire_ores");
+    public static final TagKey<Block> RUBY_ORES = create("ruby_ores");
+    public static final TagKey<Block> SILICON_ORES = create("silicon_ores");
+    public static final TagKey<Block> Z_CRYSTAL_ORES = create("z_crystal_ores");
+    public static final TagKey<Block> FOSSIL_ORES = create("fossil_ores");
+    public static final TagKey<Block> ULTRA_DARK_LOGS = create("ultra_dark_logs");
+    public static final TagKey<Block> ULTRA_JUNGLE_LOGS = create("ultra_jungle_logs");
+    public static final TagKey<Block> GHOST_LOGS = create("ghost_logs");
+    public static final TagKey<Block> CHARGE_STONE_BRICKS = create("charge_stone_bricks");
+    public static final TagKey<Block> VOLCANIC_STONE_BRICKS = create("volcanic_stone_bricks");
+    public static final TagKey<Block> POKEBALL_CHESTS = create("pokeball_chests");
 
-    private static TagKey<Block> create(ResourceLocation name) {
-        return TagKey.create(Registries.BLOCK, name);
+    private static TagKey<Block> create(String name) {
+        return TagKey.create(Registries.BLOCK, GenerationsCore.id(name));
     }
 
 }
