485d006f5af3d4abd6a9ba2ced14b5b425510682
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-05-17 16:08:46 -0400

    Refactor to Generations nameing scheme (Boat renderers)
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- common/src/main/java/generations/gg/generations/core/generationscore/client/render/entity/GenerationsChestBoatRenderer.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/client/render/entity/GenerationsChestBoatRenderer.java
@@ -3,7 +3,7 @@ package generations.gg.generations.core.generationscore.client.render.entity;
 import com.google.common.collect.ImmutableMap;
 import com.mojang.datafixers.util.Pair;
 import generations.gg.generations.core.generationscore.GenerationsCore;
-import generations.gg.generations.core.generationscore.world.entity.GenerationsBoatEntity;
+import generations.gg.generations.core.generationscore.world.entity.GenerationsChestBoatEntity;
 import net.minecraft.client.model.BoatModel;
 import net.minecraft.client.model.ListModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
@@ -16,20 +16,19 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Map;
 import java.util.stream.Stream;
 
-public class PokeModBoatRenderer extends BoatRenderer {
-    private final Map<GenerationsBoatEntity.Type, Pair<ResourceLocation, ListModel<Boat>>> boatResources;
+public class GenerationsChestBoatRenderer extends BoatRenderer {
+    private final Map<GenerationsChestBoatEntity.Type, Pair<ResourceLocation, ListModel<Boat>>> boatResources;
 
-    public PokeModBoatRenderer(EntityRendererProvider.Context context) {
+    public GenerationsChestBoatRenderer(EntityRendererProvider.Context context) {
         super(context, false);
         this.shadowRadius = 0.8F;
-        this.boatResources = Stream.of(GenerationsBoatEntity.Type.values()).collect(ImmutableMap.toImmutableMap((p_173938_) -> p_173938_,
-                (type) -> Pair.of(GenerationsCore.id("textures/entity/boat/"+ type.getName() + ".png"),
-                        new BoatModel(context.bakeLayer(new ModelLayerLocation(new ResourceLocation("minecraft", "boat/oak"), "main"))))));
+        this.boatResources = Stream.of(GenerationsChestBoatEntity.Type.values()).collect(ImmutableMap.toImmutableMap((p_173938_) -> p_173938_,
+                (type) -> Pair.of(GenerationsCore.id("textures/entity/chest_boat/"+ type.getName() + ".png"),
+                        new BoatModel(context.bakeLayer(new ModelLayerLocation(new ResourceLocation("minecraft", "chest_boat/oak"), "main"))))));
     }
 
     public Pair<ResourceLocation, ListModel<Boat>> getModelWithLocation(@NotNull Boat boat) {
-        if(boat instanceof GenerationsBoatEntity modBoat)
-            return this.boatResources.get(modBoat.getModBoatType());
+        if(boat instanceof GenerationsChestBoatEntity modBoat) return this.boatResources.get(modBoat.getModBoatType());
         return null;
     }
 }
