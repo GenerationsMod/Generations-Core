bc32c535e98955c056dadd9aff4f874187b9d3aa
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2023-05-22 12:14:20 -0400

    Add normal Build WorkFlow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
+
+plugins {
+    id("com.gradle.enterprise") version "3.13.1"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
 pluginManagement {
     repositories {
         maven("https://maven.fabricmc.net/")
