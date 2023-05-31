ee4d321ad7ac96391903085841f0308518cbdaf9
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-05-30 22:08:41 -0400

    Use GenerationsBlockTypes in GenerationsWood.java
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- common/src/main/java/generations/gg/generations/core/generationscore/network/GenerationsNetworking.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/network/GenerationsNetworking.java
@@ -1,23 +1,14 @@
 package generations.gg.generations.core.generationscore.network;
 
 import dev.architectury.networking.NetworkChannel;
-import dev.architectury.networking.NetworkManager;
 import generations.gg.generations.core.generationscore.GenerationsCore;
 import generations.gg.generations.core.generationscore.network.packets.C2SToggleCookingPotPacket;
-
-import java.util.function.BiConsumer;
-import java.util.function.Supplier;
 
 public class GenerationsNetworking {
     public static NetworkChannel NETWORK = NetworkChannel.create(GenerationsCore.id("networking"));
     
     public static void init() {
-        NETWORK.register(C2SToggleCookingPotPacket.class, C2SToggleCookingPotPacket::encode, C2SToggleCookingPotPacket::new, new BiConsumer<C2SToggleCookingPotPacket, Supplier<NetworkManager.PacketContext>>() {
-            @Override
-            public void accept(C2SToggleCookingPotPacket c2SToggleCookingPotPacket, Supplier<NetworkManager.PacketContext> packetContextSupplier) {
-                c2SToggleCookingPotPacket.process(packetContextSupplier);
-            }
-        });
+        NETWORK.register(C2SToggleCookingPotPacket.class, C2SToggleCookingPotPacket::encode, C2SToggleCookingPotPacket::new, C2SToggleCookingPotPacket::process);
     }
 
     public static void sendPacket(Object packet) {
