66ebc22fc8ef96520a67ebccfd87c079336b50af
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-06-06 14:12:10 -0400

    Fix Build
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -49,9 +49,8 @@ dependencies {
     modRuntimeOnly("lol.bai:badpackets:fabric-${project.properties["badPackets"]}")
 
     //Cobblemon
-    modApi("com.bedrockk:molang:1.1.2")
-    modImplementation("cobblemon:Cobblemon-Fabric:1.4.0") { exclude(group = "local.com.bedrockk") }
-
+    //modApi("com.bedrockk:molang:1.1.2")
+    //modApi("cobblemon:Cobblemon-Fabric:1.4.0") { exclude(group = "local.com.bedrockk") }
 }
 
 tasks {
