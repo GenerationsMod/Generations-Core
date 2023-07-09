2002d3fd9295351e5b79b79378f086e5205b1ab0
Waterpicker <waterpickerenternity@gmail.com>
2023-07-09 17:58:02 -0500

    It runs

--- common/src/main/java/generations/gg/generations/core/generationscore/world/item/creativetab/GenerationsCreativeTabs.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/world/item/creativetab/GenerationsCreativeTabs.java
@@ -10,7 +10,11 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
 
 /**
  * Generations Creative Tabs
@@ -20,28 +24,30 @@ import org.jetbrains.annotations.NotNull;
 public class GenerationsCreativeTabs {
 
     public static DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(GenerationsCore.MOD_ID, Registries.CREATIVE_MODE_TAB);
-    public static RegistrySupplier<CreativeModeTab> POKEBALLS = create("pokeballs", GenerationsItems.POKE_BALL.get());
-    public static RegistrySupplier<CreativeModeTab> RESTORATION = create("restoration", GenerationsItems.POTION.get());
+    public static RegistrySupplier<CreativeModeTab> POKEBALLS = create("pokeballs", () -> GenerationsItems.POKE_BALL);
+    public static RegistrySupplier<CreativeModeTab> RESTORATION = create("restoration", () -> GenerationsItems.POTION);
     //public static CreativeModeTab TMS = create("tms", () -> GenerationsItems.TM_1);
-    public static RegistrySupplier<CreativeModeTab> BADGES = create("badges", GenerationsItems.RAINBOW_BADGE.get());
-    public static RegistrySupplier<CreativeModeTab> RIBBONS = create("ribbons", GenerationsItems.COOLNESS_MASTER_RIBBON.get());
-    public static RegistrySupplier<CreativeModeTab> HELD_ITEMS = create("held_items", GenerationsItems.AMULET_COIN.get());
-    public static RegistrySupplier<CreativeModeTab> PLAYER_ITEMS = create("player_items", GenerationsItems.POKEDEX.get());
-    public static RegistrySupplier<CreativeModeTab> LEGENDARY_ITEMS = create("legendary_items", GenerationsItems.DNA_SPLICERS.get());
-    public static RegistrySupplier<CreativeModeTab> BUILDING_BLOCKS = create("building_blocks", GenerationsBlocks.LIGHT_BLUE_POKE_BRICK.get().asItem());
-    public static RegistrySupplier<CreativeModeTab> DECORATIONS = create("decorations", GenerationsDecorationBlocks.SWITCH.get().asItem());
-    public static RegistrySupplier<CreativeModeTab> NATURAL = create("natural", GenerationsOres.ALUMINUM_ORE.get().asItem());
-    public static RegistrySupplier<CreativeModeTab> UTILITY = create("utility", GenerationsUtilityBlocks.RED_PC.get().asItem());
-    public static RegistrySupplier<CreativeModeTab> FORM_ITEMS = create("form_items", GenerationsItems.METEORITE.get());
-    public static RegistrySupplier<CreativeModeTab> POKEMAIL = create("pokemail", GenerationsItems.POKEMAIL_AIR.get());
-    public static RegistrySupplier<CreativeModeTab> VALUABLES = create("valuables", GenerationsItems.STRANGE_SOUVENIR.get());
-    public static RegistrySupplier<CreativeModeTab> POKEDOLLS = create("pokedolls", GenerationsPokeDolls.PIKACHU_POKEDOLL.get().asItem());
-    public static RegistrySupplier<CreativeModeTab> CUISINE = create("cuisine", GenerationsItems.GIGANTAMIX.get());
-    public static RegistrySupplier<CreativeModeTab> UNIMPLEMENTED = create("unimplemented", GenerationsItems.ABILITY_URGE.get());
-    public static RegistrySupplier<CreativeModeTab> SHRINES = create("shrines", GenerationsShrines.FROZEN_SHRINE.get().asItem());
+    public static RegistrySupplier<CreativeModeTab> BADGES = create("badges", () -> GenerationsItems.RAINBOW_BADGE);
+    public static RegistrySupplier<CreativeModeTab> RIBBONS = create("ribbons", () -> GenerationsItems.COOLNESS_MASTER_RIBBON);
+    public static RegistrySupplier<CreativeModeTab> HELD_ITEMS = create("held_items", () -> GenerationsItems.AMULET_COIN);
+    public static RegistrySupplier<CreativeModeTab> PLAYER_ITEMS = create("player_items", () -> GenerationsItems.POKEDEX);
+    public static RegistrySupplier<CreativeModeTab> LEGENDARY_ITEMS = create("legendary_items", () -> GenerationsItems.DNA_SPLICERS);
+    public static RegistrySupplier<CreativeModeTab> BUILDING_BLOCKS = create("building_blocks", () -> GenerationsBlocks.LIGHT_BLUE_POKE_BRICK);
+    public static RegistrySupplier<CreativeModeTab> DECORATIONS = create("decorations", () -> GenerationsDecorationBlocks.SWITCH);
+    public static RegistrySupplier<CreativeModeTab> NATURAL = create("natural", () -> GenerationsOres.ALUMINUM_ORE);
+    public static RegistrySupplier<CreativeModeTab> UTILITY = create("utility", () -> GenerationsUtilityBlocks.RED_PC);
+    public static RegistrySupplier<CreativeModeTab> FORM_ITEMS = create("form_items", () -> GenerationsItems.METEORITE);
+    public static RegistrySupplier<CreativeModeTab> POKEMAIL = create("pokemail", () -> GenerationsItems.POKEMAIL_AIR);
+    public static RegistrySupplier<CreativeModeTab> VALUABLES = create("valuables", () -> GenerationsItems.STRANGE_SOUVENIR);
+    public static RegistrySupplier<CreativeModeTab> POKEDOLLS = create("pokedolls", () -> GenerationsPokeDolls.PIKACHU_POKEDOLL);
+    public static RegistrySupplier<CreativeModeTab> CUISINE = create("cuisine", () -> GenerationsItems.GIGANTAMIX);
+    public static RegistrySupplier<CreativeModeTab> UNIMPLEMENTED = create("unimplemented", () -> GenerationsItems.ABILITY_URGE);
+    public static RegistrySupplier<CreativeModeTab> SHRINES = create("shrines", () -> GenerationsShrines.FROZEN_SHRINE);
 
-    private static RegistrySupplier<CreativeModeTab> create(String name, @NotNull Item item) {
-        return CREATIVE_TABS.register(name, () -> CreativeTabRegistry.create(Component.translatable(name + "." + GenerationsCore.MOD_ID), item::getDefaultInstance));
+    private static <T extends ItemLike> RegistrySupplier<CreativeModeTab> create(String name, @NotNull Supplier<Supplier<T>> item) {
+        return CREATIVE_TABS.register(name, () -> CreativeTabRegistry.create(builder -> {
+            builder.title(Component.translatable(name + "." + GenerationsCore.MOD_ID)).icon(() -> item.get().get().asItem().getDefaultInstance());
+        }));
     }
 
     public static void init() {
