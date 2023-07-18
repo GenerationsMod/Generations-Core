10f04caff47b1be062e18dcc9754f01a66139fff
Waterpicker <waterpickerenternity@gmail.com>
2023-07-18 02:54:04 -0500

    Held Item support. (Needs testing.)

--- common/src/main/java/generations/gg/generations/core/generationscore/GenerationsCore.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/GenerationsCore.java
@@ -8,7 +8,9 @@
 
 package generations.gg.generations.core.generationscore;
 
+import com.cobblemon.mod.common.api.Priority;
 import com.cobblemon.mod.common.api.data.DataProvider;
+import com.cobblemon.mod.common.api.pokemon.helditem.HeldItemProvider;
 import com.mojang.logging.LogUtils;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.InteractionEvent;
@@ -26,6 +28,7 @@ import generations.gg.generations.core.generationscore.world.level.block.entitie
 import generations.gg.generations.core.generationscore.world.sound.GenerationsSounds;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -34,6 +37,7 @@ import net.minecraft.world.entity.player.Player;
 import org.slf4j.Logger;
 
 import java.nio.file.Path;
+import java.util.function.Function;
 
 /**
  * The Main Class of the Generations-Core mod. (Common)
