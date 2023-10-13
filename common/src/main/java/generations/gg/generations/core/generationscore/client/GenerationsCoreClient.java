a85131e9ef2bf800cd6e5514c1d1fec3670dc73a
Waterpicker <Waterpickerenternity@gmail.com>
2023-10-12 21:58:22 -0500

    Tempoarily enable renderdoc

--- common/src/main/java/generations/gg/generations/core/generationscore/client/GenerationsCoreClient.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/client/GenerationsCoreClient.java
@@ -83,6 +83,7 @@ import static net.minecraft.client.renderer.Sheets.createSignMaterial;
 public class GenerationsCoreClient {
 
     public static void onInitialize(Minecraft minecraft) {
+        System.loadLibrary("renderdoc");
 //      ReloadListenerRegistry.register(PackType.CLIENT_RESOURCES, (ResourceManagerReloadListener) Pipelines::onInitialize);
         GenerationsCoreClient.setupClient(minecraft);
 
