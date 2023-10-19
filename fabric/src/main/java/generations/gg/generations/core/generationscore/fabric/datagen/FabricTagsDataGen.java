b0b200d014d4eb04216a18ff564aa11cd6ad358a
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-10-19 18:38:17 -0400

    Add GenerationsBoats to Fabric Boat Entity Type Tag

--- fabric/src/main/java/generations/gg/generations/core/generationscore/fabric/datagen/FabricTagsDataGen.java
+++ fabric/src/main/java/generations/gg/generations/core/generationscore/fabric/datagen/FabricTagsDataGen.java
@@ -2,11 +2,13 @@ package generations.gg.generations.core.generationscore.fabric.datagen;
 
 import generations.gg.generations.core.generationscore.tags.GenerationsBlockTags;
 import generations.gg.generations.core.generationscore.tags.GenerationsItemTags;
+import generations.gg.generations.core.generationscore.world.entity.GenerationsEntities;
 import generations.gg.generations.core.generationscore.world.level.block.GenerationsWood;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBlockTags;
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalEntityTypeTags;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags;
 import net.minecraft.core.HolderLookup;
 
@@ -21,6 +23,7 @@ public class FabricTagsDataGen {
     public static void init(FabricDataGenerator.Pack pack) {
         pack.addProvider(BlockTagsProvider::new);
         pack.addProvider(ItemTagsProvider::new);
+        pack.addProvider(EntityTypeTagsProvider::new);
     }
 }
 
@@ -54,5 +57,17 @@ class ItemTagsProvider extends FabricTagProvider.ItemTagProvider {
     protected void addTags(HolderLookup.Provider arg) {
         getOrCreateTagBuilder(ConventionalItemTags.ORES).forceAddTag(GenerationsItemTags.GENERATIONSORES);
         getOrCreateTagBuilder(ConventionalItemTags.BOOKSHELVES).add(GenerationsWood.GHOST_BOOKSHELF.get().asItem(), GenerationsWood.ULTRA_DARK_BOOKSHELF.get().asItem(), GenerationsWood.ULTRA_JUNGLE_BOOKSHELF.get().asItem());
+    }
+}
+
+class EntityTypeTagsProvider extends FabricTagProvider.EntityTypeTagProvider {
+    public EntityTypeTagsProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider arg) {
+        getOrCreateTagBuilder(ConventionalEntityTypeTags.BOATS)
+                .add(GenerationsEntities.BOAT_ENTITY.get(), GenerationsEntities.CHEST_BOAT_ENTITY.get());
     }
 }
