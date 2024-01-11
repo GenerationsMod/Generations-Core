88d1e48c68dcc040894038cfda420b65e4deae73
Waterpicker <waterpickerenternity@gmail.com>
2024-01-11 08:07:38 -0600

    Use RenderSystem's active texture method so it knows.

--- common/src/main/java/generations/gg/generations/core/generationscore/client/render/rarecandy/Texture.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/client/render/rarecandy/Texture.java
@@ -1,16 +1,21 @@
 package generations.gg.generations.core.generationscore.client.render.rarecandy;
 
 import com.mojang.blaze3d.platform.NativeImage;
+import com.mojang.blaze3d.systems.RenderSystem;
 import gg.generations.rarecandy.pokeutils.reader.TextureReference;
 import gg.generations.rarecandy.renderer.loading.ITexture;
 import it.unimi.dsi.fastutil.io.FastByteArrayInputStream;
 import it.unimi.dsi.fastutil.io.FastByteArrayOutputStream;
 import net.minecraft.client.renderer.texture.DynamicTexture;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 import org.lwjgl.opengl.GL13C;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+
+import static org.lwjgl.opengl.GL13C.GL_TEXTURE0;
 
 public class Texture extends DynamicTexture implements ITexture {
     public Texture(TextureReference pixels) {
@@ -30,7 +35,7 @@ public class Texture extends DynamicTexture implements ITexture {
     public void bind(int slot) {
         assert slot >= 0 && slot <= 31;
 
-        GL13C.glActiveTexture('蓀' + slot);
+        RenderSystem.activeTexture(GL_TEXTURE0 + slot);
         bind();
     }
 }
