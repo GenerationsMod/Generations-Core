71c92a7ebcc3fa1a55549b3959ba9b3ab38c2782
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-01-28 16:10:38 -0500

    Fix Publishing
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
    (cherry picked from commit 4676e206895b9fd80161d6452f4c7cbc8e6466ed)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -47,9 +47,15 @@ publishing {
             url = uri(if (project.version.toString().endsWith("SNAPSHOT") || project.version.toString().startsWith("0")) snapshotsRepoUrl else releasesRepoUrl)
             name = "Generations-Repo"
             credentials {
-                username = project.properties["repoLogin"]?.toString()
-                password = project.properties["repoPassword"]?.toString()
+                username = getGensCredentials().first
+                password = getGensCredentials().second
             }
         }
     }
+}
+
+private fun getGensCredentials(): Pair<String?, String?> {
+    val username = (project.findProperty("gensUsername") ?: System.getenv("GENS_USERNAME") ?: "") as String?
+    val password = (project.findProperty("gensPassword") ?: System.getenv("GENS_PASSWORD") ?: "") as String?
+    return Pair(username, password)
 }
