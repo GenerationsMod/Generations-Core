2ecd61817df1459b614bc475f9e85771f30179f8
Waterpicker <waterpickerenternity@gmail.com>
2024-06-12 17:15:06 -0500

    2.8.1 rare candy.

--- common/src/main/java/generations/gg/generations/core/generationscore/client/CompiledModelLoader.kt
+++ common/src/main/java/generations/gg/generations/core/generationscore/client/CompiledModelLoader.kt
@@ -3,6 +3,7 @@ package generations.gg.generations.core.generationscore.client
 import generations.gg.generations.core.generationscore.client.render.rarecandy.ModelRegistry
 import net.minecraft.server.packs.resources.ResourceManager
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener
+import org.apache.commons.compress.archivers.sevenz.SevenZFile
 
 class CompiledModelLoader : ResourceManagerReloadListener {
 
@@ -11,6 +12,5 @@ class CompiledModelLoader : ResourceManagerReloadListener {
         GenerationsTextureLoader.initialize(resourceManager)
         ModelRegistry.LOADER.invalidateAll()
         ModelRegistry.LOADER.cleanUp()
-
     }
 }
