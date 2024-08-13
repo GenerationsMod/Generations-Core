d5070a75270535cec70f57cddc657fab0aa120f0
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-08-13 18:28:08 -0400

    Loom 1.7 and Gradle 8.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import net.fabricmc.loom.api.LoomGradleExtensionAPI
 plugins {
     id("architectury-plugin") version "3.4-SNAPSHOT"
     kotlin("jvm") version ("1.9.23")
-    id("dev.architectury.loom") version "1.6-SNAPSHOT" apply false
+    id("dev.architectury.loom") version "1.7-SNAPSHOT" apply false
     java
     idea
 }
