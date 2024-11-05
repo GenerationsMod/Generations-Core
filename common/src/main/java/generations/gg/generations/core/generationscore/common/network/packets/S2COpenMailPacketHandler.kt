885b7ee2516f1480c82ddd7c053f689a94a2b13d
Waterpicker <waterpickerenternity@gmail.com>
2024-11-05 00:47:41 -0600

    Fixed mail crashing packet being sent. Need to simplify code later.

--- common/src/main/java/generations/gg/generations/core/generationscore/common/network/packets/S2COpenMailPacketHandler.kt
+++ common/src/main/java/generations/gg/generations/core/generationscore/common/network/packets/S2COpenMailPacketHandler.kt
@@ -1,5 +1,7 @@
 package generations.gg.generations.core.generationscore.common.network.packets
 
+import dev.architectury.utils.Env
+import dev.architectury.utils.EnvExecutor
 import generations.gg.generations.core.generationscore.common.client.screen.mails.MailEditScreen
 import generations.gg.generations.core.generationscore.common.client.screen.mails.MailViewScreen
 import generations.gg.generations.core.generationscore.common.network.ClientNetworkPacketHandler
@@ -8,17 +10,21 @@ import net.minecraft.client.Minecraft
 
 object S2COpenMailPacketHandler : ClientNetworkPacketHandler<S2COpenMailPacket> {
     override fun handle(packet: S2COpenMailPacket, minecraft: Minecraft) {
-        val itemStack = Minecraft.getInstance().player!!.getItemInHand(packet.hand)
-        if (itemStack.`is`(GenerationsItemTags.CLOSED_POKEMAIL)) Minecraft.getInstance().setScreen(
-            MailViewScreen(
-                MailViewScreen.WrittenMailAccess(itemStack)
-            )
-        ) else if (itemStack.`is`(GenerationsItemTags.POKEMAIL)) Minecraft.getInstance().setScreen(
-            MailEditScreen(
-                Minecraft.getInstance().player,
-                itemStack,
-                packet.hand
-            )
-        )
+        EnvExecutor.runInEnv(Env.CLIENT) {
+            Runnable {
+                val itemStack = Minecraft.getInstance().player!!.getItemInHand(packet.hand)
+                if (itemStack.`is`(GenerationsItemTags.CLOSED_POKEMAIL)) Minecraft.getInstance().setScreen(
+                    MailViewScreen(
+                        MailViewScreen.WrittenMailAccess(itemStack)
+                    )
+                ) else if (itemStack.`is`(GenerationsItemTags.POKEMAIL)) Minecraft.getInstance().setScreen(
+                    MailEditScreen(
+                        Minecraft.getInstance().player,
+                        itemStack,
+                        packet.hand
+                    )
+                )
+            }
+        }
     }
 }
