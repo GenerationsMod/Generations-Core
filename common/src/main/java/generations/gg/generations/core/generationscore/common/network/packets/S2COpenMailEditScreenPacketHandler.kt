28f5820dc9c2aa7a2f302f833a2815e812d61f78
Waterpicker <waterpickerenternity@gmail.com>
2024-12-11 19:47:47 -0600

    Attempt at fixing mail crash.

--- common/src/main/java/generations/gg/generations/core/generationscore/common/network/packets/S2COpenMailEditScreenPacketHandler.kt
+++ common/src/main/java/generations/gg/generations/core/generationscore/common/network/packets/S2COpenMailEditScreenPacketHandler.kt
@@ -10,13 +10,9 @@ import net.minecraft.client.Minecraft
 object S2COpenMailEditScreenPacketHandler : ClientNetworkPacketHandler<S2COpenMailEditScreenPacket> {
     override fun handle(packet: S2COpenMailEditScreenPacket, minecraft: Minecraft) {
         val itemStack = Minecraft.getInstance().player!!.getItemInHand(packet.hand)
-        if (itemStack.`is`(GenerationsItemTags.CLOSED_POKEMAIL)) Minecraft.getInstance()
-            .setScreen(MailViewScreen(WrittenMailAccess(itemStack))) else if (itemStack.`is`(GenerationsItemTags.POKEMAIL)) Minecraft.getInstance()
-            .setScreen(
-                MailEditScreen(
-                    Minecraft.getInstance().player, itemStack,
-                    packet.hand
-                )
-            )
+        if (itemStack.`is`(GenerationsItemTags.CLOSED_POKEMAIL))
+            Minecraft.getInstance().setScreen(MailViewScreen(WrittenMailAccess(itemStack)))
+        else if (itemStack.`is`(GenerationsItemTags.POKEMAIL))
+            Minecraft.getInstance().setScreen(MailEditScreen(Minecraft.getInstance().player, itemStack, packet.hand))
     }
 }
