82bf4b8040e52fcea7c20d644c9a4836d3f09857
Conno2429 <connozebeast@gmail.com>
2025-05-30 11:37:28 -0700

    initial commit

---
+++ common/src/main/java/generations/gg/generations/core/generationscore/common/event/HeldItemFormeChange.java
@@ -0,0 +1,4 @@
+package generations.gg.generations.core.generationscore.common.event;
+
+public class HeldItemFormeChange {
+}
---
+++ common/src/main/java/generations/gg/generations/core/generationscore/common/mixin/TeraPacketExceptionMixin.java
@@ -0,0 +1,4 @@
+package generations.gg.generations.core.generationscore.common.mixin;
+
+public class TeraPacketExceptionMixin {
+}
