059692a54d7daebb67dd21b19452c080917c046f
Waterpicker <waterpickerenternity@gmail.com>
2025-11-23 19:47:14 -0600

    Hopeful Fix for a pokemon on shoulder renderer.

--- common/src/main/java/generations/gg/generations/core/generationscore/common/mixin/client/PokemonOnShoulderRenderMixin.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/common/mixin/client/PokemonOnShoulderRenderMixin.java
@@ -2,12 +2,12 @@ package generations.gg.generations.core.generationscore.common.mixin.client;
 
 import com.cobblemon.mod.common.client.render.layer.PokemonOnShoulderRenderer;
 import com.cobblemon.mod.common.client.render.models.blockbench.PosableModel;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
 //import generations.gg.generations.core.generationscore.common.client.render.entity.GenerationsPokemonOnShoulderProxy;
 //import generations.gg.generations.core.generationscore.common.client.render.entity.GenerationsPokemonOnShoulderProxy;
-import generations.gg.generations.core.generationscore.common.client.render.entity.GenerationsPokemonOnShoulderProxy;
+import generations.gg.generations.core.generationscore.common.client.model.RareCandyBone;
+//import generations.gg.generations.core.generationscore.common.client.render.entity.GenerationsPokemonOnShoulderProxy;
 import generations.gg.generations.core.generationscore.common.client.render.entity.PokemonOnShoulderRenderAccess;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -19,7 +19,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.UUID;
@@ -30,13 +30,22 @@ public abstract class PokemonOnShoulderRenderMixin<T extends Player> extends Ren
         super(renderer);
     }
 
-    @Invoker(value = "extractUuid", remap = false) public abstract UUID invokeExtractUuid(CompoundTag tag);
+//    @Invoker(value = "extractUuid", remap = false) public abstract UUID invokeExtractUuid(CompoundTag tag);
+//
+//    @Invoker(value = "extractData", remap = false) public abstract PokemonOnShoulderRenderer.ShoulderData shoulderDataFrom(CompoundTag shoulderNbt, UUID pokemonUUID);
 
-    @Invoker(value = "extractData", remap = false) public abstract PokemonOnShoulderRenderer.ShoulderData invokeExtractData(CompoundTag shoulderNbt, UUID pokemonUUID);
+    @Redirect(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/player/Player;FFFFFFZ)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V"))
+    public void renderTranslate(PoseStack instance, double x, double y, double z, @Local(argsOnly = true) boolean pLeftShoulder, @Local(name = "model") PosableModel model) {
+        if(model.getRootPart() instanceof RareCandyBone) {
+            x += pLeftShoulder ? -0.175 : 0.175;
+        }
 
-    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/player/Player;FFFFFFZ)V", at = @At("HEAD"), cancellable = true)
-    public void render(PoseStack matrixStack, MultiBufferSource buffer, int packedLight, T livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, boolean pLeftShoulder, CallbackInfo ci) {
-        GenerationsPokemonOnShoulderProxy.render(this, matrixStack, buffer, packedLight, livingEntity, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, pLeftShoulder);
-        ci.cancel();
+        instance.translate(x, y, z);
     }
+
+//    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/player/Player;FFFFFFZ)V", at = @At("HEAD"), cancellable = true)
+//    public void render(PoseStack matrixStack, MultiBufferSource buffer, int packedLight, T livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, boolean pLeftShoulder, CallbackInfo ci) {
+//        GenerationsPokemonOnShoulderProxy.render(this, matrixStack, buffer, packedLight, livingEntity, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, pLeftShoulder);
+//        ci.cancel();
+//    }
 }
