992e328ca5785146bdc557580513d216ebce3107
Waterpicker <waterpickerenternity@gmail.com>
2025-12-16 20:30:21 -0600

    RareCandy 2.14.2!

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
 
 //    modImplementation(group = "earth.terrarium.common_storage_lib", name = "common-storage-lib-common-1.21.1", version = "0.0.7")
 
-    implementation(files("../libs/RareCandy-2.14.1.jar"))
+    implementation(files("../libs/RareCandy-2.14.2.jar"))
 
 //    implementation("gg.generations:RareCandy:${project.properties["rareCandy"]}"){isTransitive = false}
 
--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
 
     modLocalRuntime("me.djtheredstoner:DevAuth-fabric:${project.properties["devauth_version"]}")
 
-    "shadowBundle"(implementation(files("../libs/RareCandy-2.14.1.jar"))!!)
+    "shadowBundle"(implementation(files("../libs/RareCandy-2.14.2.jar"))!!)
 //    implementation("shadowBundle"("gg.generations", "RareCandy", "${project.properties["rareCandy"]}") {isTransitive = false})!!
 
     implementation("shadowBundle"("com.github.Chocohead:Fabric-ASM:v2.3")!!)
