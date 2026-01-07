afaf30f1aef54504454146f2054124f57bedfe76
Waterpicker <waterpickerenternity@gmail.com>
2026-01-07 01:10:14 -0600

    change

--- common/src/main/java/generations/gg/generations/core/generationscore/common/client/render/rarecandy/RareCandyVertexArray.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/common/client/render/rarecandy/RareCandyVertexArray.java
@@ -2,55 +2,75 @@ package generations.gg.generations.core.generationscore.common.client.render.rar
 
 import org.lwjgl.opengl.GL43;
 
-public class RareCandyVertexArray {
+public final class RareCandyVertexArray {
+
     private final int vao;
     private final int vertexStride;
-    
+
+    // binding index used for all vertex attributes
+    private static final int VERTEX_BINDING = 0;
+
     public RareCandyVertexArray(int strideBytes) {
         this.vertexStride = strideBytes;
         this.vao = GL43.glGenVertexArrays();
-        
         setupAttributes();
     }
-    
+
     private void setupAttributes() {
         GL43.glBindVertexArray(vao);
-        
-        // Position (vec3)
+
+        // Position (vec3) @ location 0
         GL43.glEnableVertexAttribArray(0);
-        GL43.glVertexAttribPointer(0, 3, GL43.GL_FLOAT, false, vertexStride, 0);
-        
-        // Color (vec4)
+        GL43.glVertexAttribFormat(0, 3, GL43.GL_FLOAT, false, 0);
+        GL43.glVertexAttribBinding(0, VERTEX_BINDING);
+
+        // Color (vec4) @ location 1
         GL43.glEnableVertexAttribArray(1);
-        GL43.glVertexAttribPointer(1, 4, GL43.GL_FLOAT, false, vertexStride, 12);
-        
-        // UV0 (vec2)
+        GL43.glVertexAttribFormat(1, 4, GL43.GL_FLOAT, false, 12);
+        GL43.glVertexAttribBinding(1, VERTEX_BINDING);
+
+        // UV0 (vec2) @ location 2
         GL43.glEnableVertexAttribArray(2);
-        GL43.glVertexAttribPointer(2, 2, GL43.GL_FLOAT, false, vertexStride, 28);
-        
-        // UV1 (ivec2)
+        GL43.glVertexAttribFormat(2, 2, GL43.GL_FLOAT, false, 28);
+        GL43.glVertexAttribBinding(2, VERTEX_BINDING);
+
+        // UV1 (ivec2) @ location 3
         GL43.glEnableVertexAttribArray(3);
-        GL43.glVertexAttribIPointer(3, 2, GL43.GL_INT, vertexStride, 36);
-        
-        // UV2 (ivec2)
+        GL43.glVertexAttribIFormat(3, 2, GL43.GL_INT, 36);
+        GL43.glVertexAttribBinding(3, VERTEX_BINDING);
+
+        // UV2 (ivec2) @ location 4
         GL43.glEnableVertexAttribArray(4);
-        GL43.glVertexAttribIPointer(4, 2, GL43.GL_INT, vertexStride, 44);
-        
-        // Normal (vec3)
+        GL43.glVertexAttribIFormat(4, 2, GL43.GL_INT, 44);
+        GL43.glVertexAttribBinding(4, VERTEX_BINDING);
+
+        // Normal (vec3) @ location 5
         GL43.glEnableVertexAttribArray(5);
-        GL43.glVertexAttribPointer(5, 3, GL43.GL_FLOAT, false, vertexStride, 52);
-        
+        GL43.glVertexAttribFormat(5, 3, GL43.GL_FLOAT, false, 52);
+        GL43.glVertexAttribBinding(5, VERTEX_BINDING);
+
         GL43.glBindVertexArray(0);
     }
-    
+
+    /** Bind VAO only */
     public void bind() {
         GL43.glBindVertexArray(vao);
     }
-    
+
+    /** Bind the actual vertex buffer (destBuffer) at draw time */
+    public void bindVertexBuffer(int bufferId) {
+        GL43.glBindVertexBuffer(
+                VERTEX_BINDING,
+                bufferId,
+                0,
+                vertexStride
+        );
+    }
+
     public void unbind() {
         GL43.glBindVertexArray(0);
     }
-    
+
     public void delete() {
         GL43.glDeleteVertexArrays(vao);
     }
--- common/src/main/java/generations/gg/generations/core/generationscore/common/client/render/rarecandy/loading/GenerationsMultiRenderObject.java
+++ common/src/main/java/generations/gg/generations/core/generationscore/common/client/render/rarecandy/loading/GenerationsMultiRenderObject.java
@@ -45,43 +45,63 @@ public class GenerationsMultiRenderObject extends MultiRenderObject {
     @Override
     public void render(RenderStage renderStage, List<ObjectInstance> list) {
         updateSSBOs();
+
+        System.out.println("vao=" + Pipelines.vao);
         Pipelines.vao.bind();
 
+        System.out.println("glBindBuffer ARRAY_BUFFER destBuffer=" + destBuffer);
         RenderSystem.glBindBuffer(GL43.GL_ARRAY_BUFFER, destBuffer);
 
         for (int i = 0; i < instances.size(); i++) {
+            System.out.println("instanceIndex=" + i);
             Pipelines.transformVertices(this, i);
 
             var instance = instances.get(i);
+
             for (int mesh = 0; mesh < meshes.length; mesh++) {
-                if (shouldRender(mesh, instance)) {
-                    var model = this.meshes[mesh];
+                System.out.println("meshIndex=" + mesh);
 
-                    if (model == null) {
-                        continue;
-                    }
-
-
-                    System.out.println("modelBuffer: " + modelBuffer);
-                    System.out.println("destBuffer: " + destBuffer);
-                    System.out.println("instanceBuffer.bufferId: " + instanceBuffer.getBufferId());
-                    System.out.println("uvTransformBuffer.bufferId: " + uvTransformBuffer.getBufferId());
-
-                    Pipelines.processMaterial(instance, this, mesh);
-
-                    Pipelines.commonOn();
-
-                    Pipelines.solidOn();
-                    model.render();
-                    Pipelines.solidOff();
-
-                    Pipelines.emissiveOn();
-                    model.render();
-                    Pipelines.emissiveOff();
+                if (!shouldRender(mesh, instance)) {
+                    System.out.println("skipRender mesh=" + mesh);
+                    continue;
                 }
+
+                var model = this.meshes[mesh];
+                System.out.println("model=" + model);
+
+                if (model == null) {
+                    System.out.println("model NULL mesh=" + mesh);
+                    continue;
+                }
+
+                System.out.println("modelBuffer=" + modelBuffer);
+                System.out.println("destBuffer=" + destBuffer);
+                System.out.println("instanceBufferId=" + instanceBuffer.getBufferId());
+                System.out.println("uvTransformBufferId=" + uvTransformBuffer.getBufferId());
+
+                System.out.println("processMaterial");
+                Pipelines.processMaterial(instance, this, mesh);
+
+                System.out.println("commonOn");
+                Pipelines.commonOn();
+
+                System.out.println("solidOn");
+                Pipelines.solidOn();
+                System.out.println("model.render SOLID");
+                model.render();
+                System.out.println("solidOff");
+                Pipelines.solidOff();
+
+                System.out.println("emissiveOn");
+                Pipelines.emissiveOn();
+                System.out.println("model.render EMISSIVE");
+                model.render();
+                System.out.println("emissiveOff");
+                Pipelines.emissiveOff();
             }
         }
     }
+
 
     //TODO: calculate
     @Override
